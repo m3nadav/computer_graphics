67387eb28b84000bd5ba10a8d1a36ec3134e0dbb
Nadav Mermer <m3nadav@gmail.com>
2025-07-28 20:10:30 +0300

    Fix compilation

--- maman17/run_visual_test.sh
+++ maman17/run_visual_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Compiling and running visual test..."
-g++ -std=c++11 -I./include -framework OpenGL -framework GLUT src/visual_test.cpp src/cow/spots.cpp src/cow/body.cpp src/cow/cow.cpp src/shapes/shapes.cpp -o visual_test_working
+g++ -std=c++11 -I./include -framework OpenGL -framework GLUT src/visual_test.cpp src/cow/spots.cpp src/cow/body.cpp src/cow/cow.cpp src/cow/head.cpp src/shapes/shapes.cpp -o visual_test_working
+if [ $? -ne 0 ]; then
+    echo "Error compiling visual test"
+    exit 1
+fi
 ./visual_test_working 
